b48fcc90e21efb87df6372b7555c3d4b3ea3bb0c
Leonardo Hansa <leonardo.torres@conento.com>
2018-04-22 20:12:08 +0200

    Pruebas con las frecuencias

--- datascienceplus.R
+++ datascienceplus.R
@@ -1,3 +1,4 @@
+library(audio)
 library(tuneR)
 
 # https://datascienceplus.com/recording-and-measuring-your-musical-progress-with-r/
@@ -7,36 +8,34 @@ setWavPlayer("/usr/bin/play")
 
 originalSound <- readWave("G-scale.wav")
 play(originalSound) 
+plot(originalSound) 
 
 ## Extract the notes ------------------------------------------------------
 
-scaleNotesFreqs <- c(NA, NA, NA, 196.00, 196.00, NA, 220.0, NA, NA, 246.9, NA, 261.6, 261.6, NA, 293.7, 293.7, NA, 329.6, 329.6, NA, 370.0, 370.0, NA, 392.0, NA)
-scaleNotes <- noteFromFF(scaleNotesFreqs)
-
-
-
-transcribeMusic(originalSound, expNotes = scaleNotes) <- c(NA, NA, NA, 196.00, 196.00, NA, 220.0, NA, NA, 246.9, NA, 261.6, 261.6, NA, 293.7, 293.7, NA, 329.6, 329.6, NA, 370.0, 370.0, NA, 392.0, NA)
-scaleNotes <- noteFromFF(scaleNotesFreqs)
-
 source("src/transcribeMusic.R")
+
+scaleNotesFreqs <- c(NA, NA, NA, 196.00, 196.00, NA, 220.0, NA, NA, 246.9, NA, 261.6, 261.6, NA, 293.7, 293.7, NA, 329.6, 329.6, NA, 370.0, 370.0, NA, 392.0, NA)
+# scaleNotesFreqs <- c(NA, NA, NA, 392.0, 392.0, NA, 220.0, NA, NA, 246.9, NA, 261.6, 261.6, NA, 293.7, 293.7, NA, 329.6, 329.6, NA, 370.0, 370.0, NA, 392.0, NA)
+scaleNotes <- noteFromFF(scaleNotesFreqs)
 
 transcribeMusic(originalSound, expNotes = scaleNotes)
 
 ## Grabarse ---------------------------------------------------------
 
-# install.packages("audio")
-library(audio)
-
 audiorec <- function(kk,f){  # kk: time length in seconds; f: filename
-  if(f %in% list.files()) 
-  {file.remove(f); print('The former file has been replaced');}
+  
+  if(f %in% list.files()){
+    file.remove(f); print('The former file has been replaced')
+  }
   require(audio)
   s11 <- rep(NA_real_, 16000*kk) # Samplingrate=16000
   message("5 seconds..") # Counting down 5 seconds befor the recording starts
+  
   for (i in c(5:1)){
     message(i)
     Sys.sleep(1)
   }
+  
   message("Recording starts now...")
   record(s11, 16000, 1)  # record in mono mode
   wait(kk)
@@ -44,22 +43,18 @@ audiorec <- function(kk,f){  # kk: time length in seconds; f: filename
   .rs.restartR() # As mentioned in the above cited post: recording with the audio package works once, but for some reason it will not continue to work afterwards unless the R session is restarted. For this reason I included a restart in this function. I am hoping to find a more elegant solution one day soon.
 }
 
-
-
-
-
 ## Pruebas -----------------------------------------------------------
+
 onda <- readWave("Kurzweil-K2000-Dual-Bass-C1.wav")
 
 play(onda)
 plot(onda)
 
+head(onda@left)
+head(onda@right)
 
-
-onda@left
-onda@right
-
-mono(onda,"left")
+play(mono(onda,"left"))
+play(mono(onda,"right"))
 
 log(-onda@left[1],base = 2)+1
 
